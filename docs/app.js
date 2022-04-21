badc8ec126976541f38ba3d5b562a840bce5af1f
Daniel Nieto <dani2600nieto@gmail.com>
2022-04-20 19:03:21 -0500

    Already finish, I want to change the icons

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,8 @@
+/*I set as default Bogota */
+window.addEventListener('load', () => {
+    displayClimaticInfo('Bogota');
+});
+
 let btnTopCities = document.querySelectorAll('.btn-top-city');
 btnTopCities.forEach(button => button.addEventListener('click', selectCity));
 
@@ -27,9 +32,8 @@ function displayClimaticInfo(city){
                     return response.json();
                 })
                 .then(data => {
-                    injectDataInDOM(data);
                     currentTemperature = kelvinToCelsius(data.main.temp);
-                    console.log(currentTemperature);
+                    injectDataInDOM(city, data, currentTemperature);
                 })
         })
 }
@@ -38,6 +42,60 @@ function kelvinToCelsius(kelvinDegrees){
     return Math.ceil(kelvinDegrees - 273.15);
 }
 
-function injectDataInDOM(data){
+function injectDataInDOM(city, data, currentTemperature){
+    console.log(city);
     console.log(data);
+    console.log(currentTemperature);
+    injectCityInformation(city, data);
+    injectWeatherInfo(data, currentTemperature);
+}
+
+function injectWeatherInfo(data, currentTemperature){
+    let degree = document.querySelector('.degree-temperature');
+    degree.textContent = currentTemperature;
+    injectIcon(data);
+}
+
+function injectIcon(data){
+    let iconImg = document.querySelector('.icon-weather');
+    let icon = data.weather[0].icon;
+    iconImg.setAttribute('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
+    console.log(icon);
+}
+function injectCityInformation(city, data){
+    let cityTitle = document.querySelector('.city-title');
+    cityTitle.textContent = city;
+    let cityCountry = document.querySelector('.city-country');
+    cityCountry.textContent = data.sys.country;
+    let cityDate = document.querySelector('.city-date');
+    let cityHour = document.querySelector('.city-time');
+    setCityDateAndTime(cityDate, cityHour, data.timezone);
+    let cityWeatherDescription = document.querySelector('.city-weather-description');
+    cityWeatherDescription.textContent = data.weather[0].main;
+}
+
+function setCityDateAndTime(containerDate, containerHour, offset) {
+    /*Here we make the convertion of offset from seconds to hours */
+    let offsetHours = (offset / 3600).toLocaleString();
+    /*We create a new Date Object*/
+    let d = new Date();
+    /*Convert to miliseconds
+      Subtract local time zone offset
+      Get UTC time in miliseconds*/
+    let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    console.log(utc);
+    /*Create new Date object for different city and Using supplied offset in hours*/
+    let nd = new Date(utc + (3600000*offsetHours));
+    setCityDate(containerDate, nd);
+    /*Get the hour in text*/
+    let cityHourText = `${nd.getHours().toString().padStart(2, "0")} : ${nd.getMinutes().toString().padStart(2, "0")}`;
+    /*Inject*/
+    containerHour.textContent = cityHourText;
+}
+
+function setCityDate(container, date){
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let cityDateText = `${days[date.getDay()]},  ${date.getDate()} ${months[date.getMonth()]}`
+    container.textContent = cityDateText;
 }
